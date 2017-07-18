ca98cb51119f2732a940b848fc9e42ffbb1519cd
%= GitConfig.user_name % <%= GitConfig.user_email %>
2017-07-18 09:21:53 -0700

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, :password_digest, presence: true
-    validates :password length: { minimum: 6, allow_nil: true}
+    validates :password, length: { minimum: 6, allow_nil: true}
     after_initialize :ensure_session_token
 
     attr_reader :password
