5e86b476e463b078609b20272ec19e79beab8b7d
Geewon Lee <gwlee1991@gmail.com>
2017-07-21 09:23:22 -0700

    add new migration to add new column image url to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720004446) do
+ActiveRecord::Schema.define(version: 20170721161902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170720004446) do
     t.string   "session_token"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "image_url"
   end
 
 end
