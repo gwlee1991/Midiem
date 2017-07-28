074b0c75235a3dab19769c835ea1a1e9a1f5dad0
Geewon Lee <gwlee1991@gmail.com>
2017-07-27 22:37:29 -0700

    reset seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,24 +11,9 @@ Post.destroy_all
 Comment.destroy_all
 
 guest = User.create(username: "guest", password: "password", image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/c_scale,w_32/v1501014959/default-user-image_gvyan3.png")
-user = User.create(username: "midiemiscool", password: "password", image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/c_scale,w_297/v1501116609/pexels-photo-487600_qjky6o.jpg")
 
-
-
+topic1 = Topic.create(title: "Most popular")
 topic2 = Topic.create(title: "Technology")
 topic3 = Topic.create(title: "Travel")
 topic4 = Topic.create(title: "Parenting")
 topic1 = Topic.create(title: "E-Sports")
-
-post1 = Post.create(title: "Faker wins again", body: "Faker wins like always", author_id: guest.id, topic_id: topic1.id, image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/v1500508529/faker_haj29z.jpg")
-post2 = Post.create(title: "SKT wins again", body: "SKT wins again like always", author_id: guest.id, topic_id: topic1.id, image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/v1500578757/SKT1_vjei8b.png")
-post3 = Post.create(title: "Iphone 7 is mediocre", body: "I don't feel like Iphone 7 is good as 6", author_id: user.id, topic_id: topic2.id, image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/v1500582530/phone_nmzslm.jpg")
-post4 = Post.create(title: "Is mac better than pc?", body: "Is mac better than pc as a programmer view?", author_id: user.id, topic_id: topic2.id, image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/c_scale,w_1681/v1500582536/laptop_czoar6.jpg")
-post5 = Post.create(title: "Traveling is fun", body: "beautiful pictures pictures", author_id: user.id, topic_id: topic3.id, image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/v1500676315/travel1_g4mzwc.jpg")
-post6 = Post.create(title: "Traveling is awesome", body: "you get to see beautiful places", author_id: user.id, topic_id: topic3.id, image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/v1500676334/travel2_ktq8x4.jpg")
-post7 = Post.create(title: "Parenting is scary", body: "It's best when the baby is still in the womb", author_id: guest.id, topic_id: topic4.id, image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/v1500676294/parenting1_gfpmjn.jpg")
-post8 = Post.create(title: "Parenting is awesome", body: "Kids are so fun", author_id: guest.id, topic_id: topic4.id, image_url: "http://res.cloudinary.com/dbtdkqyeo/image/upload/v1500676307/parenting2_eyv4we.jpg")
-
-comment1 = Comment.create(body: "hello", post_id: post1.id, user_id: guest.id);
-comment2 = Comment.create(body: "hello", post_id: post2.id, user_id: guest.id);
-comment2 = Comment.create(body: "bye", post_id: post1.id, user_id: guest.id);
