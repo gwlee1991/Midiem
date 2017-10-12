11cbcbe992741bf42100b9217629f3871695d5b3
Geewon Lee <gwlee1991@gmail.com>
2017-10-12 12:30:41 -0700

    drop a table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011021009) do
+ActiveRecord::Schema.define(version: 20171012192933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,13 +21,6 @@ ActiveRecord::Schema.define(version: 20171011021009) do
     t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "followers", force: :cascade do |t|
-    t.integer  "follower_id", null: false
-    t.integer  "followee_id", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "follows", force: :cascade do |t|
